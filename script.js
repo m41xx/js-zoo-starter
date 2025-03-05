e62ed176c1a86e926dc13f764c3b0197ca2e0a83
m41xx <miasieminski@students.berkeley.net>
2025-03-05 16:34:32 +0000

    Done!

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-//starter code
-
 function run() {
-    sleep("Tigger");
+  var tigger = new Tiger("Tigger");
+  tigger.eat("meat");
+  tigger.eat("kibble");
+}
+
+class Tiger {
+  constructor(name) {
+    this.name = name;
+    this.favoriteFood = "meat";
   }
-  
-  function sleep(name) {
-    console.log(name + " sleeps for 8 hours")
+
+  sleep() {
+    console.log(this.name + " sleeps for 8 hours");
   }
+
+  eat(food) {
+    console.log(this.name + " eats " + food);
+    if (food === this.favoriteFood) {
+      console.log("YUM!! " + this.name + " wants more " + food);
+    } else {
+      this.sleep();
+    }
+  }
+}
